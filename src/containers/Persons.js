5db6ab7df860e9fff22d843d5d6207ec3d749cb6
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-11-09 15:38:25 +0900

    replace local state with redux

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import * as actionTypes from '../store/actions';
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
+  // state = {
+  //     persons: []
+  // }
+  //
+  // personAddedHandler = () => {
+  //     const newPerson = {
+  //         id: Math.random(), // not really unique but good enough here!
+  //         name: 'Max',
+  //         age: Math.floor( Math.random() * 40 )
+  //     }
+  //     this.setState( ( prevState ) => {
+  //         return { persons: prevState.persons.concat(newPerson)}
+  //     } );
+  // }
+  //
+  // personDeletedHandler = (personId) => {
+  //     this.setState( ( prevState ) => {
+  //         return { persons: prevState.persons.filter(person => person.id !== personId)}
+  //     } );
+  // }
 
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
-
-    render () {
-        return (
-            <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
-                    <Person
-                        key={person.id}
-                        name={person.name}
-                        age={person.age}
-                        clicked={() => this.personDeletedHandler(person.id)}/>
-                ))}
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <AddPerson personAdded={this.props.onAddPerson} />
+        {this.props.storedPersons.map(person => (
+          <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            clicked={() => this.props.onDeletePerson(person.id)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Persons;
+// Redux code below
+const mapStateToProps = state => {
+  return {
+    storedPersons: state.persons
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
+    onDeletePerson: (personID) => dispatch({type: actionTypes.DELETE_PERSON, id: personID})
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
